6b12d82f08bcc3a47a068c3f51cc15c4099ee96b
Gareth Jones <oxidising@gmail.com>
2018-11-12 18:19:38 +0000

    Fixed main.cpp corruption

--- main.cpp
+++ main.cpp
@@ -2,20 +2,6 @@
 
 int main()
 {
-<<<<<<< HEAD
-	// Select the types we want to use
-	using result_type = std::uint64_t;
-	using state_type = std::uint64_t;
-
-	// Select the generator
-	const generator_type selection = generator_type::xoro128;
-
-	// The number of random numbers we want to generate
-	const size_t n_rands = 1e8;
-
-	// How many threads to use
-	const unsigned int n_threads = 8;
-=======
 	// Select the generator
 	// Available generators are the xoro128, pcg and jsf PRNGs
 	const generator_type selection = generator_type::jsf;
@@ -49,15 +35,9 @@ int main()
 	my_generator.generate_2D(vector_storage);
 	
 	auto t_end = hr_clock::now();
->>>>>>> devel
 
-	// Somewhere to store them
-	std::vector<std::uint64_t> rand_vec(n_rands);
+	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t_end - t_start);
 
-<<<<<<< HEAD
-	// Instantiate the Threaded_rands object
-	Threaded_rands<result_type, state_type> my_generator(selection, n_threads);
-=======
 	std::cout << "\nIt took " << duration.count() << " ms to fill " << n_threads << " vectors of " << n_rands << " rands.\n";
 	
 	std::cout << "\nA sample of random numbers : \n\n";
@@ -68,8 +48,5 @@ int main()
 	}
 
 	std::cout << "\nFinished generating.\n";
->>>>>>> devel
 
-	// Fill the vector
-	my_generator.generate(vector_storage);
-}
+} // End main

bbd26fa0c08be78b8b17c195fb1e8625e576e813
Gareth Jones <oxidising@gmail.com>
2018-11-12 09:45:58 +0000

    Good morning

--- main.cpp
+++ main.cpp
@@ -2,34 +2,25 @@
 
 int main()
 {
-	const generator_type selection = generator_type::xoro128;
-
-	const size_t n_rands = 1e8;
-	const unsigned int n_threads = 8;
-
+	// Select the types we want to use
 	using result_type = std::uint64_t;
 	using state_type = std::uint64_t;
 
-	// A big vector of vectors
-	std::vector<std::vector<uint64_t>> vector_storage;
+	// Select the generator
+	const generator_type selection = generator_type::xoro128;
 
-	// Lets make some big-ish vectors	
-	for(int i = 0; i < n_threads; i++)
-		vector_storage.emplace_back(std::vector<uint64_t>(n_rands));
+	// The number of random numbers we want to generate
+	const size_t n_rands = 1e8;
 
+	// How many threads to use
+	const unsigned int n_threads = 8;
+
+	// Somewhere to store them
+	std::vector<std::uint64_t> rand_vec(n_rands);
+
+	// Instantiate the Threaded_rands object
 	Threaded_rands<result_type, state_type> my_generator(selection, n_threads);
 
-	// How long does it take
-	using hr_clock = std::chrono::high_resolution_clock;
-	
-	auto t_start  = hr_clock::now();
-
-	my_generator.generate_2D(vector_storage);
-
-	auto t_end = hr_clock::now();
-
-	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t_end - t_start);
-
-	std::cout << duration.count() << " ms to fill " << n_threads << " vectors of " << n_rands << " rands with the xoro128 generator.\n";
-
-} // End main
+	// Fill the vector
+	my_generator.generate(vector_storage);
+}
